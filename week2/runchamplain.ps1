df61f41d539ca8f9f3c8be24c704b5a26a5719ba
AdieStevens <adie.stevens@mymail.champlain.edu>
2024-01-26 11:15:38 -0500

    Run Champlain on Chrome Script

---
+++ week2/runchamplain.ps1
@@ -0,0 +1,9 @@
+$chromeProcess = Get-Process "chrome" -ErrorAction SilentlyContinue
+
+if ($chromeProcess -eq $null) {
+	Start-Process "chrome" -Argumentlist "https://www.champlain.edu"
+	}
+else {
+	Stop-Process -Name "chrome" -Force
+}
+
