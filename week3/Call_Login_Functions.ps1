1ca2676797348049379b90bbe492e581c73cc36c
AdieStevens <adie.stevens@mymail.champlain.edu>
2024-02-08 18:19:12 -0500

    Week 3 Scripts

---
+++ week3/Call_Login_Functions.ps1
@@ -0,0 +1,15 @@
+﻿. (Join-Path $PSScriptRoot "login_records.ps1")
+
+clear
+
+# Get Login and logoffs from the last 15 days
+$loginoutsTable = Get-LoginLogoutRecords -NumberOfDays 15
+$loginoutsTable
+
+# Get Shut Downs from the last 25 days 
+$shutdownsTable = Get-ComputerStartShutdownRecords -NumberOfDays 25 | Where-Object { $_.Event -eq "Shutdown" }
+$shutdownsTable
+
+# Get start ups from the last 25 days 
+$startupsTable = Get-ComputerStartShutdownRecords -NumberOfDays 25 | Where-Object { $_.Event -eq "Start" }
+$startupsTable
