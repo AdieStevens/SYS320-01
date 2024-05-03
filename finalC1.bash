ca2a2bf77e7387f27222914a5a00ba25a72c3a3b
AdieStevens <90576946+AdieStevens@users.noreply.github.com>
2024-05-02 20:13:08 -0400

    Create finalC1.bash

---
+++ finalC1.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+fullPage=$(curl -sL "http://10.0.17.5/IOC.html")
+
+ioc=$(echo "$fullPage" | \
+xmlstarlet select --template --copy-of "//html//body//table/tr")
+
+echo $ioc | sed 's/<\/tr>/\n/g' | \
+             sed -e 's/&amp;//g' | \
+             sed -e 's/<tr>//g' | \
+			 sed -e 's/<td[^>]*>//g' | \
+	         sed -e 's/<\/td>/;/g' | \
+             sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
+             sed -e 's/<[/\]\{0,1\}nobr>//g' | \
+			 grep -v "Pattern" | \
+			 cut -d';' -f1 | \
+			 sed 's/ //' | \
+			 sed '/^$/d' \
+			 > IOC.txt
